ed025eed8150fd4508c28247a2c6f07f308c7993
Keerthi Siva <keerthi@swiftmedical.io>
2020-02-19 23:42:28 -0500

    add searching for players by name

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,19 @@
 class PlayersController < ApplicationController
   def index
-    @players = serilaized_resouce
+    @players = serilaized_resouce(Player.all)
     render component: 'Players', props: { players: @players }
+  end
+
+  def search
+    players = Player.where("lower(name) LIKE ?", "%#{params[:name]}%")
+    @players = serilaized_resouce(players)
+    render :json => { players: @players }
   end
 
   private
 
-  def serilaized_resouce
-    Player.includes(:performances).map do |player|
+  def serilaized_resouce(players)
+    players.map do |player|
       player.serializable_hash(include: [:performances, :team])
     end
   end
--- app/javascript/components/Players.js
+++ app/javascript/components/Players.js
@@ -1,17 +1,57 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 import Player from "./Player"
-class Players extends React.Component {
-  playerRows = () => {
-    return (
-      this.props.players.map(player => (<tr><Player player={player} /></tr>))
-    );
+const Players = ({ players }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const focusSearch = useRef(null);
+
+  useEffect(() => {focusSearch.current.focus()}, [])
+
+   const getPlayers = async (query) => {
+     const results = await fetch(`http://localhost:3000/search?name=${query}`, {
+       headers: {'accept': 'application/json'}
+     })
+     const playersResult = await results.json()
+     return playersResult.players
+    }
+
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
   }
 
-  render () {
+  useEffect(() => {
+    let currentQuery = true
+    const controller = new AbortController()
+
+    const loadPlayers = async () => {
+      if (!searchValue) return setSearchResults([])
+
+      await sleep(350)
+      if (currentQuery && searchValue) {
+        const playersResult = await getPlayers(searchValue, controller)
+        setSearchResults(playersResult)
+      }
+    }
+    loadPlayers()
+
+    return () => {
+      currentQuery = false
+      controller.abort()
+    }
+  }, [searchValue])
+
+  const playersList = searchResults.length == 0 ? players : searchResults
+
     return (
       <div>
         <h1> All Players </h1>
+        <input type="text"
+          type="name"
+          placeholder="Search for a Player..."
+          ref={focusSearch}
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue} />
         <table>
           <thead>
             <tr>
@@ -34,12 +74,17 @@ class Players extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.playerRows()}
+            {playerRows(playersList)}
           </tbody>
         </table>
       </div>
     );
-  }
+}
+
+const playerRows = (players) => {
+  return (
+    players.map(player => (<tr><Player player={player} /></tr>))
+  );
 }
 
 Players.propTypes = {
