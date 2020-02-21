916222f4e67ec300a4cf6748f4988f9aede72944
Keerthi Siva <keerthi@swiftmedical.io>
2020-02-20 22:51:06 -0500

    fix the issue with search without any name

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,7 +7,7 @@ class PlayersController < ApplicationController
 
   def search
     players = Player.where(nil)
-    players = players.by_name(params[:name])
+    players = players.by_name(params[:name]) if params[:name]
     if params[:order]
       order = params[:order].split(',').each_slice(2) do |order_name, dir|
         players = players.order_by_perf(order_name, dir)
