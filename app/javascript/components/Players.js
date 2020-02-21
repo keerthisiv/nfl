dd56a1bd5f3981dd544cc2c9a07286d7798cd591
Keerthi Siva <keerthi@swiftmedical.io>
2020-02-20 21:27:11 -0500

    add csv download for players

--- app/javascript/components/Players.js
+++ app/javascript/components/Players.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 import Player from "./Player"
+import { CSVLink } from "react-csv";
+
 const Players = ({ players }) => {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
@@ -11,21 +13,25 @@ const Players = ({ players }) => {
   useEffect(() => {focusSearch.current.focus()}, [])
   useEffect(() => {focusSort.current.focus()}, [])
 
-   const getPlayers = async (search, sort) => {
-     let url = "http://localhost:3000/search?";
-     if (search) {
-       url = url + `name=${search}`
-     }
-     if (Object.keys(sort).length != 0) {
-       url = url + `order=${Object.entries(sort).join(',')}`
-     }
-
-     const results = await fetch(url, {
-       headers: {'accept': 'application/json'}
-     })
-     const playersResult = await results.json()
-     return playersResult.players
+  const getPlayers = async (search, sort) => {
+    let url = "http://localhost:3000/search?";
+    if (search) {
+      url = url + `name=${search}`
     }
+    if (Object.keys(sort).length != 0) {
+      if (search) {
+        url = url + `&order=${Object.entries(sort).join(',')}`
+      } else {
+        url = url + `order=${Object.entries(sort).join(',')}`
+      }
+    }
+
+    const results = await fetch(url, {
+      headers: {'accept': 'application/json'}
+    })
+    const playersResult = await results.json()
+    return playersResult.players
+  }
 
   const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -61,6 +67,24 @@ const Players = ({ players }) => {
     }
   }
 
+  const headers = [
+    { label: "Name", key: "name" },
+    { label: "Team Name", key: "team.name" },
+    { label: "Position", key: "position" },
+    { label: "Attempts Per Game", key: "performances[0].attempts_per_game"},
+    { label: "Attempts", key: "performances[0].attempts"},
+    { label: "Total Yards", key: "performances[0].total_yards"},
+    { label: "Average Yards Per Attempt", key: "performances[0].average_yards_per_attempt"},
+    { label: "Yards Per Game", key: "performances[0].yards_per_game"},
+    { label: "Total Touch down", key: "performances[0].total_touch_down"},
+    { label: "Logest Ruch", key: "performances[0].longest_rush"},
+    { label: "First Down", key: "performances[0].first_down"},
+    { label: "First Down Percentage", key: "performances[0].first_down_percentage"},
+    { label: "Twenty Plus Rush", key: "performances[0].twenty_plus_rush"},
+    { label: "fourty Plus Rush", key: "performances[0].fourty_plus_rush"},
+    { label: "Fumbles", key: "performances[0].fumbles" }
+  ];
+
   const playersList = searchResults.length == 0 ? players : searchResults
 
   return (
@@ -72,6 +96,9 @@ const Players = ({ players }) => {
         ref={focusSearch}
         onChange={(e) => setSearchValue(e.target.value)}
         value={searchValue} />
+      <CSVLink data={playersList} headers={headers} asyncOnClick={true}>
+        Download me
+      </CSVLink>
       <table>
         <thead>
           <tr>
